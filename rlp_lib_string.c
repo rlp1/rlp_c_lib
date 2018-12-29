e9335b07df2ff8e6441d8f51ed2658518b7f2205
rlp1 <rlp001@outlook.com>
2018-12-29 11:45:31 -0200

    Add a new version of function "strsplit", using "strtok".

--- rlp_lib_string.c
+++ rlp_lib_string.c
@@ -4,11 +4,14 @@
 
 #include "rlp_lib_string.h"
 
-char* strcpy_range(const char *src, const size_t s_index, const size_t e_index) {
+char*
+strcpy_range(const char *src, const size_t s_index, const size_t e_index) 
+{
     const size_t len = strlen(src);
     char *buffer = (char *) malloc(sizeof(char) * (e_index - s_index) + 2);
     size_t buffer_pos = 0;
-    for (size_t pos = s_index; pos <= e_index; pos++, buffer_pos++) {
+    for (size_t pos = s_index; pos <= e_index; pos++, buffer_pos++) 
+    {
         *(buffer + buffer_pos) = *(src + pos);
     }
     // @Note Every string in "C" must be '\0' in the last index of the character array.
@@ -16,8 +19,49 @@ char* strcpy_range(const char *src, const size_t s_index, const size_t e_index) 
     return buffer;
 }
 
-char** strsplit(const char *src, const char token) {
-    if (src == NULL) {
+char** 
+strsplit(char *str, const char *token)
+{
+    char *current_token = strtok(str, token);
+    size_t tok_pos = 0;
+
+    char **buffer = NULL;
+
+    while (current_token != NULL)
+    {
+        // @Note Check if the buffer is null, then allocate a memory
+        // to the buffer.
+        if (buffer == NULL)
+        {
+            // @Note Allocate 1 byte size to the buffer,
+            // and in the next opertaions re-allocate it.
+            buffer = (char **) malloc(1);
+        }
+        
+        // @Note The "memcpy" function token length is represented by the all characters
+        // that contains in the token string plus 1, this one represents the every last
+        // character that contains in the every string that is represented by '\0'.
+        //      -rlp, 29 December 2018.
+        const size_t memcpy_token_length = strlen(current_token) + 1;
+
+        *(buffer + tok_pos) = (char *) malloc(memcpy_token_length);
+        memcpy(*(buffer + tok_pos), current_token, memcpy_token_length);
+
+        printf("%s\n", *(buffer + tok_pos));
+
+        current_token = strtok(NULL, token);
+        tok_pos++;
+    }
+
+    return buffer;
+}
+
+/* This function to split strings is deprecated. */
+char** 
+_____strsplit(const char *src, const char token) 
+{
+    if (src == NULL) 
+    {
         return NULL;
     }
 
@@ -27,11 +71,13 @@ char** strsplit(const char *src, const char token) {
     size_t split_pos = 0;
     size_t last_token_pos = 0;
 
-    for (size_t pos = 0; pos < len; pos++) {
+    for (size_t pos = 0; pos < len; pos++) 
+    {
         // @Note This represents the current char of the source string.
         const char current_char = *(src + pos);
 
-        if (current_char == token || pos == len - 1) {
+        if (current_char == token || pos == len - 1) 
+        {
             *(split + split_pos) = strcpy_range(src, last_token_pos, pos == len - 1 ? pos : pos - 1);
 
             // @Note The last token position is set to "pos + 1", because is must jump the token character.
--- rlp_lib_string.h
+++ rlp_lib_string.h
@@ -10,11 +10,23 @@
  * */
 char* strcpy_range(const char *src, const size_t s_index, const size_t e_index);
 
+/* This function split the stri string in parts depending by the token
+ * that is used to split this. 
+ * 
+ * src -> this represents the string that will be split.
+ * token -> represents the string that is used to split the tokens.
+*/
+char** strsplit(char *str, const char *token)
+
 /* This function split the string in parts between the "token".
+ *
+ * @Note This function is deprecated.   
  *
  * src -> this represents the string that will be split.
  * token -> this represents the token that will be checked in the string to be split.
  * */
-char** strsplit(const char *src, const char token);
+char**
+_____strsplit(const char *src, const char token);
+
 
 #endif //RLP_LIB_RLP_LIB_STRING_H
